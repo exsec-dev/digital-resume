39a8cad13fb69524649b12750b8894bd542b811b
Ekaterina Prozhirko <80840150+exsecantb@users.noreply.github.com>
2025-02-27 00:30:50 +0300

    Project codebase removed from the default branch

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from "./Logo";
-export { TimePeriodPanel } from "./TimePeriodPanel";
-export { ProjectCard } from "./ProjectCard";
-export { CollapsePanel } from "./CollapsePanel";
-export { RunningLine } from "./RunningLine";
